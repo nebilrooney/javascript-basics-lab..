ec6b2762223f084e9ad30f34f38e869afa2def8f
nebilrooney <nabilrooney123@gmail.com>
2025-01-24 05:18:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let studentName = "Don Martin";
+let studentAge = 31; 
+let isEnrolled = true; 
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+let exampleString = "nice to meet you!";
+console.log("String Example:", exampleString);
+
+let exampleNumber = 44;
+console.log("Number Example:", exampleNumber);
+
+let exampleBoolean = false;
+console.log("Boolean Example:", exampleBoolean);
+
+let exampleArray = "apple";
+console.log("Array Example:", exampleArray)
+
+let exampleObject = { firstName: "Don", lastName: "Martin"} ;
+console.log("Object Example:", exampleObject);
